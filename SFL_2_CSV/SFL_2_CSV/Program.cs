c01d8c8a4b255a480e2da0da2ba92683b9d5ccc4
cavemanAGz <woodal@oregonstate.edu>
2018-02-13 11:56:36 -0800

    SRML SFL to CSV program started
    Three projects created together, one fo rthe windows forms project, one class file project for file handling operationas, and a class project for the SFL parser.

---
+++ SFL_2_CSV/SFL_2_CSV/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SFL_2_CSV
+{
+     static class Program
+     {
+          /// <summary>
+          /// The main entry point for the application.
+          /// </summary>
+          [STAThread]
+          static void Main()
+          {
+               Application.EnableVisualStyles();
+               Application.SetCompatibleTextRenderingDefault(false);
+               Application.Run(new SFL_2_CSV_main_form());
+          }
+     }
+}
